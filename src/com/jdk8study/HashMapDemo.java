bd86d3667e6d1b11f4602aa338c90f53e42e4554
mingle08 <mingle08@163.com>
2022-04-25 00:39:25 +0800

    用于调试jdk

---
+++ src/com/jdk8study/HashMapDemo.java
@@ -0,0 +1,11 @@
+package com.jdk8study;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class HashMapDemo {
+    public static void main(String[] args) {
+        Map<Integer, String> map = new HashMap<>();
+        map.put(1, "First");
+    }
+}
